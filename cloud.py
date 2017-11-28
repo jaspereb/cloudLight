991145bfd0f9ec3db463f86e7b40406eb0dfbc7c
Jasper Brown <jasperebrown@gmail.com>
2017-11-28 12:02:59 +0000

    stuff

---
+++ cloud.py
@@ -0,0 +1,59 @@
+#This is the cloud global variables file
+from neopixel import *
+
+# LED strip configuration: (Thanks to Tony DiCola)
+LED_COUNT      = 120      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP      = ws.WS2811_STRIP_GRB  # Strip type and colour ordering
+
+#A list of common colours
+red = Color(255,0,0)
+green = Color(0,255,0)
+blue = Color(0,0,255)
+black = Color(0,0,0)
+white = Color(150,255,220)
+
+#Monthly average temperatures in deg C
+averages = [26,26,25,23,20,18,17,18,20,22,24,26]
+    
+#Variable Definitions
+weatherState = 'clear'
+#0 - 'clear'
+#1 - 'rain'
+#2 - 'weird'
+#3 - 'storms'
+#4 - 'extreme' 
+
+intensity = 0
+#The ranges for intensity are:
+# storm - 0-2
+# rain - 0-4
+
+temperature = 0
+#Temperature is the difference from the monthly mean in Celsius 
+#(only used for sunny/cloudy weather)
+
+mode = 'weather'
+#A mode switch for the cloud state machine
+# 'weather' displays the current weather
+# 'alarm' displays the current weather between the set alarm times every morning
+# 'lightning' displays a lightning show
+# 'rain' displays a rain show
+# 'rainbow' displays a rainbow show
+# 'color' displays a fixed colour
+# 'off' turns the lights off
+
+brightness = 0.9
+#[0-1] float which adjusts the relative brightness of all display functions
+
+color = red
+#Only applies to the 'color' mode
+
+alarmStart = [8, 20];
+alarmStop = [8, 40];
+#The alarm start and stop times as tuples of (hh,mm) in 24 hour time
---
+++ displayHelper.py
@@ -0,0 +1,92 @@
+import cloud
+import time
+import threading
+from itertools import chain
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(cloud.strip.numPixels()):
+         
+        cloud.strip.setPixelColor(i, color)
+        cloud.strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def colorSegment(segment, color):
+    if(segment == 'left'):
+        numbers = chain(range(1,33), range(67,105))
+        for i in numbers:
+            cloud.strip.setPixelColor(i, color)
+        cloud.strip.show()
+    elif(segment == 'mid'):
+        numbers = chain(range(33,38), range(62,67), range(104,111))
+        for i in numbers:
+            cloud.strip.setPixelColor(i, color)
+        cloud.strip.show()
+
+    elif(segment == 'right'):
+        numbers = chain(range(38,62), range(111,121))
+        for i in numbers:
+            cloud.strip.setPixelColor(i, color)
+        cloud.strip.show()
+
+    else:
+        print "Incorrect segment, use 'left' 'mid' 'right'"
+
+def setColor(color, wait_ms=0):
+        for i in range(cloud.strip.numPixels()):
+                cloud.strip.setPixelColor(i, color)
+                time.sleep(wait_ms/1000.0)
+        cloud.strip.show()
+
+def theaterChase(color, wait_ms=50, iterations=10):
+    """Movie theater light style chaser animation."""
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, cloud.strip.numPixels(), 3):
+                cloud.strip.setPixelColor(i+q, color)
+            cloud.strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, cloud.strip.numPixels(), 3):
+                cloud.strip.setPixelColor(i+q, 0)
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+def rainbow(wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256*iterations):
+        for i in range(cloud.strip.numPixels()):
+            cloud.strip.setPixelColor(i, wheel((i+j) & 255))
+        cloud.strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def rainbowCycle(wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256*iterations):
+        for i in range(cloud.strip.numPixels()):
+            cloud.strip.setPixelColor(i, wheel((int(i * 256 / cloud.strip.numPixels()) + j) & 255))
+        cloud.strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def theaterChaseRainbow(wait_ms=50):
+    """Rainbow movie theater light style chaser animation."""
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, cloud.strip.numPixels(), 3):
+                cloud.strip.setPixelColor(i+q, wheel((i+j) % 255))
+            cloud.strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, cloud.strip.numPixels(), 3):
+                cloud.strip.setPixelColor(i+q, 0)
+
+def goDark():
+    setColor(Color(0,0,0),0)
